a5213f3aa5d26ec5c228b1c6163a2f0b3d557f33
Steven Starnes <starnes81@gmail.com>
2017-10-28 15:59:32 -0500

    added axios and set up api call

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,3 +3,20 @@ export const FETCH_POSTS = 'fetch_posts';
 export const FETCH_POST= 'fetchPost';
 
 const ROOT_URL = 'http://blackonair.com/wp-json/wp/v2/';
+
+
+export function fetchPosts() {
+  const request = axios.get(`${ROOT_URL}/posts`);
+    return {
+      type: FETCH_POSTS,
+      payload: request
+    };
+}
+
+export function fetchPost() {
+  const request = axios.get(`${ROOT_URL}/posts/${id}`);
+    return {
+      type: FETCH_POST,
+      paylod: request
+    };
+}
